47e96eacf2515801b07046c456880db3c44380b0
Serena Chen <serenazychen@gmail.com>
2017-02-06 12:17:23 -0500

    finished exercise 2

--- exercises/ex02/stack.c
+++ exercises/ex02/stack.c
@@ -14,7 +14,7 @@ int *foo() {
     int i;
     int array[SIZE];
 
-    //printf("%p\n", array);
+    // printf("%p\n", array);
 
     for (i=0; i<SIZE; i++) {
 	array[i] = 42;
@@ -26,7 +26,7 @@ void bar() {
     int i;
     int array[SIZE];
 
-    //printf("%p\n", array);
+    // printf("%p\n", array);
 
     for (i=0; i<SIZE; i++) {
 	array[i] = i;
@@ -45,3 +45,32 @@ int main()
 
     return 0;
 }
+
+/**
+Exercise questions:
+1)  It should print 2 pointers for the arrays in foo and bar, and then print the
+    contents of the array from foo.
+
+2)  warning: function returns address of local variable [-Wreturn-local-addr]
+     return array;
+
+    C can't return the address of a local variable, since the local variable
+    will go away after the method is over ie. the pointer will point to
+    space that used to be part of the foo local space but is obsolete after
+    the foo is over.
+
+3)  Output:
+    0x7ffc82525320
+    0x7ffc82525320
+    Segmentation fault (core dumped)
+
+    The pointers of the two arrays point to exactly the same memory addresses.
+    Once a method is over, the process reuses that memory for the next thing on
+    the stack. In this case, the stack used some memory to stare the variables
+    of foo, then reused that memory for bar, which is essentially the same
+    method, so all the pieces of memory lined up, but when the process went into
+    the for loop and called printf, printf overwrote the piece of memory that
+    contained the array, and a seg fault occured.
+
+4)  There's just a Segmentation Fault. Probably for the same reason as above.
+*/
